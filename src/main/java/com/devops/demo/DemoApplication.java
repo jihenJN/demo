959a939b403de764056c68d90dbda335a3f0c33a
jihenJN <jihenjihen23@gmail.com>
2023-07-23 11:40:36 +0100

    try

--- src/main/java/com/devops/demo/DemoApplication.java
+++ src/main/java/com/devops/demo/DemoApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		int x = 1528 ;
-		int y = 1415 ; 
+		int x = 1 ;
+		int y = 1 ; 
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
