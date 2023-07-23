61b0df2cd4c48e3e6e735c5092bf00cd9afab057
jihenJN <jihenjihen23@gmail.com>
2023-07-23 11:12:57 +0100

    from rep dev

--- src/main/java/com/devops/demo/DemoApplication.java
+++ src/main/java/com/devops/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		int x = 5 ;
+		int x = 15 ;
 		int y = 10 ; 
 		SpringApplication.run(DemoApplication.class, args);
 	}
